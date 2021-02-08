ffdbd5b7f2598597e4cf5d79ea616cfdff2c36ca
Slartibarty <josh.dowell.winter@gmail.com>
2021-02-08 05:02:02 +0000

    Weeee new BSP format

--- common/bspflags.h
+++ common/bspflags.h
@@ -8,66 +8,103 @@
 // a given brush can contribute multiple content bits
 // multiple brushes can be in a single leaf
 
-// lower bits are stronger, and will eat weaker brushes completely
-#define	CONTENTS_SOLID			1		// an eye is never valid in a solid
-#define	CONTENTS_WINDOW			2		// translucent, but not watery
-#define	CONTENTS_AUX			4
-#define	CONTENTS_LAVA			8
-#define	CONTENTS_SLIME			16
-#define	CONTENTS_WATER			32
-#define	CONTENTS_MIST			64
-#define	LAST_VISIBLE_CONTENTS	CONTENTS_MIST
+enum contentFlags_t
+{
+	// lower bits are stronger, and will eat weaker brushes completely
+	CONTENTS_SOLID			= 1,			// an eye is never valid in a solid
+	CONTENTS_WINDOW			= 2,			// translucent, but not watery
+	CONTENTS_AUX			= 4,
+	CONTENTS_LAVA			= 8,
+	CONTENTS_SLIME			= 16,
+	CONTENTS_WATER			= 32,
+	CONTENTS_MIST			= 64,
+	LAST_VISIBLE_CONTENTS	= CONTENTS_MIST,
 
 // remaining contents are non-visible, and don't eat brushes
 
-#define	CONTENTS_AREAPORTAL		0x8000
+	CONTENTS_AREAPORTAL		= 0x8000,
 
-#define	CONTENTS_PLAYERCLIP		0x10000
-#define	CONTENTS_MONSTERCLIP	0x20000
+	CONTENTS_PLAYERCLIP		= 0x10000,
+	CONTENTS_MONSTERCLIP	= 0x20000,
 
 // currents can be added to any other contents, and may be mixed
-#define	CONTENTS_CURRENT_0		0x40000
-#define	CONTENTS_CURRENT_90		0x80000
-#define	CONTENTS_CURRENT_180	0x100000
-#define	CONTENTS_CURRENT_270	0x200000
-#define	CONTENTS_CURRENT_UP		0x400000
-#define	CONTENTS_CURRENT_DOWN	0x800000
+	CONTENTS_CURRENT_0		= 0x40000,
+	CONTENTS_CURRENT_90		= 0x80000,
+	CONTENTS_CURRENT_180	= 0x100000,
+	CONTENTS_CURRENT_270	= 0x200000,
+	CONTENTS_CURRENT_UP		= 0x400000,
+	CONTENTS_CURRENT_DOWN	= 0x800000,
 
-#define	CONTENTS_ORIGIN			0x1000000	// removed before bsping an entity
+	CONTENTS_ORIGIN			= 0x1000000,	// removed before bsping an entity
 
-#define	CONTENTS_MONSTER		0x2000000	// should never be on a brush, only in game
-#define	CONTENTS_DEADMONSTER	0x4000000
-#define	CONTENTS_DETAIL			0x8000000	// brushes to be added after vis leafs
-#define	CONTENTS_TRANSLUCENT	0x10000000	// auto set if any surface has trans
-#define	CONTENTS_LADDER			0x20000000
+	CONTENTS_MONSTER		= 0x2000000,	// should never be on a brush, only in game
+	CONTENTS_DEADMONSTER	= 0x4000000,
+	CONTENTS_DETAIL			= 0x8000000,	// brushes to be added after vis leafs
+	CONTENTS_TRANSLUCENT	= 0x10000000,	// auto set if any surface has trans
+	CONTENTS_LADDER			= 0x20000000
+};
+
+// Surface types
+
+constexpr int NUM_SURFACE_BITS = 4;
+constexpr int MAX_SURFACE_TYPES = 1 << NUM_SURFACE_BITS;
+
+enum surfaceTypes_t
+{
+	SURFTYPE_CONCRETE,		// default type
+	SURFTYPE_METAL,			// HL2 style solid metal
+	SURFTYPE_VENT,			// Half-Life clumpy vent
+	SURFTYPE_DIRT,			// Muddy mucky dirt, like HL1
+	SURFTYPE_GRASS,			// HL2 style grass
+	SURFTYPE_SAND,			// Grainy sand
+	SURFTYPE_ROCK,			// Cave rock
+	SURFTYPE_WOOD,			// Wood plank floor, like HL2
+	SURFTYPE_TILE,			// Slippery tile floor
+	SURFTYPE_COMPUTER,		// Sharp hardware
+	SURFTYPE_GLASS,			// Glass pane, decently thick
+	SURFTYPE_FLESH,			// Meat! Yuck!
+	SURFTYPE_12,
+	SURFTYPE_13,
+	SURFTYPE_14,
+	SURFTYPE_15
+};
 
 // Surface flags
 
-#define	SURF_LIGHT		0x1		// value will hold the light strength
+enum surfaceFlags_t
+{
+	SURF_TYPE_BIT0 = 0x1,	// First X bits are surface type parameters
+	SURF_TYPE_BIT1 = 0x2,
+	SURF_TYPE_BIT2 = 0x4,
+	SURF_TYPE_BIT3 = 0x8,
+	SURF_TYPE_MASK = ( 1 << NUM_SURFACE_BITS ) - 1,
 
-#define	SURF_SLICK		0x2		// effects game physics
+	SURF_LIGHT		= 0x10,		// value will hold the light strength
 
-#define	SURF_SKY		0x4		// don't draw, but add to skybox
-#define	SURF_WARP		0x8		// turbulent water warp
-#define	SURF_TRANS33	0x10
-#define	SURF_TRANS66	0x20
-#define	SURF_FLOWING	0x40	// scroll towards angle
-#define	SURF_NODRAW		0x80	// don't bother referencing the texture
+	SURF_SLICK		= 0x20,		// affects game physics
 
-// From utils qfiles.h
-#define	SURF_HINT		0x100	// make a primary bsp splitter
-#define	SURF_SKIP		0x200	// completely ignore, allowing non-closed brushes
+	SURF_SKY		= 0x40,		// don't draw, but add to skybox
+	SURF_WARP		= 0x80,		// turbulent water warp
+	SURF_TRANS33	= 0x100,
+	SURF_TRANS66	= 0x200,
+	SURF_FLOWING	= 0x400,	// scroll towards angle
+	SURF_NODRAW		= 0x800,	// don't bother referencing the texture
+
+	// From utils qfiles.h
+	SURF_HINT		= 0x1000,	// make a primary bsp splitter
+	SURF_SKIP		= 0x2000,	// completely ignore, allowing non-closed brushes
+};
 
 // Content masks
 
-#define	MASK_ALL				(-1)
-#define	MASK_SOLID				(CONTENTS_SOLID|CONTENTS_WINDOW)
-#define	MASK_PLAYERSOLID		(CONTENTS_SOLID|CONTENTS_PLAYERCLIP|CONTENTS_WINDOW|CONTENTS_MONSTER)
-#define	MASK_DEADSOLID			(CONTENTS_SOLID|CONTENTS_PLAYERCLIP|CONTENTS_WINDOW)
-#define	MASK_MONSTERSOLID		(CONTENTS_SOLID|CONTENTS_MONSTERCLIP|CONTENTS_WINDOW|CONTENTS_MONSTER)
-#define	MASK_WATER				(CONTENTS_WATER|CONTENTS_LAVA|CONTENTS_SLIME)
-#define	MASK_OPAQUE				(CONTENTS_SOLID|CONTENTS_SLIME|CONTENTS_LAVA)
-#define	MASK_SHOT				(CONTENTS_SOLID|CONTENTS_MONSTER|CONTENTS_WINDOW|CONTENTS_DEADMONSTER)
+#define MASK_ALL				(-1)
+#define MASK_SOLID				(CONTENTS_SOLID|CONTENTS_WINDOW)
+#define MASK_PLAYERSOLID		(CONTENTS_SOLID|CONTENTS_PLAYERCLIP|CONTENTS_WINDOW|CONTENTS_MONSTER)
+#define MASK_DEADSOLID			(CONTENTS_SOLID|CONTENTS_PLAYERCLIP|CONTENTS_WINDOW)
+#define MASK_MONSTERSOLID		(CONTENTS_SOLID|CONTENTS_MONSTERCLIP|CONTENTS_WINDOW|CONTENTS_MONSTER)
+#define MASK_WATER				(CONTENTS_WATER|CONTENTS_LAVA|CONTENTS_SLIME)
+#define MASK_OPAQUE				(CONTENTS_SOLID|CONTENTS_SLIME|CONTENTS_LAVA)
+#define MASK_SHOT				(CONTENTS_SOLID|CONTENTS_MONSTER|CONTENTS_WINDOW|CONTENTS_DEADMONSTER)
 #define MASK_CURRENT			(CONTENTS_CURRENT_0|CONTENTS_CURRENT_90|CONTENTS_CURRENT_180|CONTENTS_CURRENT_270|CONTENTS_CURRENT_UP|CONTENTS_CURRENT_DOWN)
 
 // Surface masks
--- common/q_formats.h
+++ common/q_formats.h
@@ -580,18 +580,18 @@ namespace wad2
 
 constexpr int IDBSPHEADER = MakeID('I', 'B', 'S', 'P');
 
-#define BSPVERSION	38
+#define BSPVERSION	39
 
 // upper design bounds
 // leaffaces, leafbrushes, planes, and verts are still bounded by
 // 16 bit short limits
-#define	MAX_MAP_MODELS		1024
-#define	MAX_MAP_BRUSHES		8192
-#define	MAX_MAP_ENTITIES	2048
+#define	MAX_MAP_MODELS		2048	// 1024
+#define	MAX_MAP_BRUSHES		16384	// 8192
+#define	MAX_MAP_ENTITIES	4096	// 2048
 #define	MAX_MAP_ENTSTRING	0x40000
 #define	MAX_MAP_TEXINFO		8192
 
-#define	MAX_MAP_AREAS		256
+#define	MAX_MAP_AREAS		512		// 256
 #define	MAX_MAP_AREAPORTALS	1024
 #define	MAX_MAP_PLANES		65536
 #define	MAX_MAP_NODES		65536
