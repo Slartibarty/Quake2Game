2bcb3e2fa5acbfe517758295d3f1f43b2dc53b46
Slartibarty <josh.dowell.winter@gmail.com>
2021-03-10 16:10:13 +0000

    fabs thing

--- engine/shared/cmodel.cpp
+++ engine/shared/cmodel.cpp
@@ -1261,9 +1261,9 @@ void CM_RecursiveHullCheck (int num, float p1f, float p2f, vec3_t p1, vec3_t p2)
 			if (trace_ispoint)
 				offset = 0.0f;
 			else
-				offset = fabsf(trace_extents[0]*plane->normal[0]) +
-					fabsf(trace_extents[1]*plane->normal[1]) +
-					fabsf(trace_extents[2]*plane->normal[2]);
+				offset = fabs(trace_extents[0]*plane->normal[0]) +
+					fabs(trace_extents[1]*plane->normal[1]) +
+					fabs(trace_extents[2]*plane->normal[2]);
 		}
 
 		// see which sides we need to consider
