031a0a28ae29658ae3a694db750b4d83867d418e
Slartibarty <josh.dowell.winter@gmail.com>
2021-06-16 22:28:14 +0100

    New resources

---
+++ winres/iconsrc/makeicon.bat
@@ -0,0 +1,3 @@
+rem done this way so the ico is in order from largest to smallest
+magick convert engine-256.png engine-64.bmp engine-48.bmp engine-40.bmp engine-32.bmp engine-24.bmp engine-20.bmp engine-16.bmp ../engine.ico
+pause
